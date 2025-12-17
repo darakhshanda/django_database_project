7b3904c471208ed3a105f8c61531e909d15f64cc
darakhshanda <darakhshanda@hotmail.com>
2025-12-17 14:48:36 +0000

    Setup Virtual envirnment, create .gitignore  and env.py files

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q!&-+_(mu!v5hi3tean7h0qfmc7^2q^3gz(bd+7)$aff_u_bdu'
+if os.path.isfile('env.py'):
+    import env
+    DEBUG = True
+else:
+    DEBUG = False
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

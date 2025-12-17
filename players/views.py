0c9b0abf4735ca5c7b7f75cfd7eaa7d6828d55a5
darakhshanda <darakhshanda@hotmail.com>
2025-12-17 17:11:34 +0000

    Create player.html in templates for player app

--- players/views.py
+++ players/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 
 def players(request):
-    return HttpResponse("Players works!")
+    template = loader.get_template('players.html')
+    return HttpResponse(template.render())

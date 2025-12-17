4ce683d45232b7ced497b8d3ea855ae6ac32259b
darakhshanda <darakhshanda@hotmail.com>
2025-12-17 18:02:28 +0000

    Create classes for player and Quest in model.py

--- players/models.py
+++ players/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Player(models.Model):
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    age = models.IntegerField()
+    username = models.CharField(max_length=64, unique=True)
+
+
+class Quest(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    difficulty = models.CharField(max_length=20)
+    initiated = models.BooleanField(default=False)
+    timer = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+    player = models.ForeignKey(
+        Player, on_delete=models.CASCADE, related_name='quests')
